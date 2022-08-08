725d86dfa3a0947dff5c6ec9eae7ef5ac55dc992
Taha Toy <mttoy45@gmail.com>
2022-08-08 14:35:40 +0300

    continue

---
+++ DataAccess/Concrete/RandevuContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete
+{
+    public class RandevuContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=MhrsDatabase;Trusted_Connection=true;");
+
+        }
+
+        public DbSet<User> User { get; set; }
+        public DbSet<Patient> Patient { get; set; }
+        public DbSet<Doctor> Doctor { get; set; }
+        public DbSet<Appointment> Appointment { get; set; }
+
+    }
+}
